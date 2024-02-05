dbee53add6fd1631489bb7dae2dffa54ed230d44
Mridul1703 <mridulvasudeva.03@gmail.com>
2024-02-06 01:27:13 +0530

    changed favicon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from 'react'
+import { React, useState } from 'react'
 import axios from 'axios';
 import WeatherDetails from './components/WeatherDetails';
 import weatherlogo from './components/weather.svg';
