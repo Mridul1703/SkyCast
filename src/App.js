08ac8de4f2aceffb1bc86f6f23ca6cab6c6b2e1f
Mridul1703 <mridulvasudeva.03@gmail.com>
2024-04-12 15:25:18 +0530

    final check!

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ export const App = () => {
     <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
         <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={weatherlogo} class="h-8" alt="SkyCast Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">SkyCast - AutoOps</span>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">SkyCast - AutoOps!</span>
         </a>
     </div>
 </nav>
